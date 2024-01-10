70e6cde6539d4e5739515ac138885db50f0bd2c2
ihsan215 <aliihsantas34@gmail.com>
2024-01-10 14:56:33 +0300

    added hello world

---
+++ 001/hello/target/debug/deps/hello.d
@@ -0,0 +1,5 @@
+C:\Users\aliih\Desktop\ihsan\BlockChain\rust\Rust-Tutorial\001\hello\target\debug\deps\hello.exe: src\main.rs
+
+C:\Users\aliih\Desktop\ihsan\BlockChain\rust\Rust-Tutorial\001\hello\target\debug\deps\hello.d: src\main.rs
+
+src\main.rs:
---
+++ 001/hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+C:\Users\aliih\Desktop\ihsan\BlockChain\rust\Rust-Tutorial\001\hello\target\debug\hello.exe: C:\Users\aliih\Desktop\ihsan\BlockChain\rust\Rust-Tutorial\001\hello\src\main.rs
